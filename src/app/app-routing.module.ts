bbfc9b0f93f7aae0fb7cbc8c0b108bf397b60507
lwalik <l.walicki1@gmail.com>
2022-11-08 15:52:44 +0100

    feat: categoriesListComponent created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/products-list/product-list.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { ProductListComponentModule } from './components/products-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { CategoriesListComponentModule } from './components/categories-list/categories-list.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
+      { path: 'categories', component: CategoriesListComponent },
     ]),
     ProductListComponentModule,
     ProductsServiceModule,
+    CategoriesListComponentModule,
+    CategoriesServiceModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
