f05c2d061d6a086659ef65cca39b8f4ead381f9f
lwalik <l.walicki1@gmail.com>
2022-11-28 00:14:34 +0100

    feat(advanced-lists-ex-4): sort by salary added

--- src/app/components/advanced-employees-list/advanced-employees-list.component-module.ts
+++ src/app/components/advanced-employees-list/advanced-employees-list.component-module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { FlexModule } from '@angular/flex-layout/flex';
 import { AdvancedEmployeesListComponent } from './advanced-employees-list.component';
 
 @NgModule({
-  imports: [MatCardModule, MatListModule, CommonModule],
+  imports: [MatCardModule, MatListModule, CommonModule, FlexModule],
   declarations: [AdvancedEmployeesListComponent],
   providers: [],
   exports: [AdvancedEmployeesListComponent]
--- src/app/components/advanced-employees-list/advanced-employees-list.component.ts
+++ src/app/components/advanced-employees-list/advanced-employees-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
+import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
 import { EmployeeModel } from '../../models/employee.model';
 import { EmployeesService } from '../../services/employees.service';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-advanced-employees-list',
@@ -10,8 +11,24 @@ import { EmployeesService } from '../../services/employees.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdvancedEmployeesListComponent {
-  readonly employees$: Observable<EmployeeModel[]> = this._employeesService.getAll();
+  private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>('asc');
+  public order$: Observable<string> = this._orderSubject.asObservable();
+  public orders$: Observable<string[]> = of(['asc', 'desc']);
+  readonly employees$: Observable<EmployeeModel[]> = combineLatest([
+    this._employeesService.getAll(),
+    this.order$
+  ]).pipe(
+    map(([employees, order]: [EmployeeModel[], string]) => employees.sort((a, b) => {
+      if (a.salary > b.salary) return order === 'asc' ? 1 : -1;
+      if (a.salary < b.salary) return order === 'asc' ? -1 : 1;
+      return 0;
+    }))
+  )
 
   constructor(private _employeesService: EmployeesService) {
+  }
+
+  sort(order: string): void {
+    this._orderSubject.next(order);
   }
 }
