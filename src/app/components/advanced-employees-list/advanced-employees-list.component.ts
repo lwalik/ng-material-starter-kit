cae3fa356011e7cd43bf9d9310db911cb1b93922
lwalik <l.walicki1@gmail.com>
2022-11-28 00:36:44 +0100

    feat(advanced-lists-ex-4): filter by age added

--- src/app/components/advanced-employees-list/advanced-employees-list.component.ts
+++ src/app/components/advanced-employees-list/advanced-employees-list.component.ts
@@ -1,34 +1,79 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { EmployeeModel } from '../../models/employee.model';
 import { EmployeesService } from '../../services/employees.service';
-import {map} from "rxjs/operators";
+
+interface AgeRange {
+  minAge: string;
+  maxAge: string;
+}
 
 @Component({
   selector: 'app-advanced-employees-list',
   templateUrl: './advanced-employees-list.component.html',
   encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AdvancedEmployeesListComponent {
-  private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>('asc');
+  private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
+    'asc'
+  );
   public order$: Observable<string> = this._orderSubject.asObservable();
   public orders$: Observable<string[]> = of(['asc', 'desc']);
+
+  private _ageRangeSubject: BehaviorSubject<AgeRange> =
+    new BehaviorSubject<AgeRange>({
+      minAge: '0',
+      maxAge: '100',
+    });
+  public ageRange$: Observable<AgeRange> = this._ageRangeSubject.asObservable();
+  public ageRanges$: Observable<string[]> = of([
+    'All',
+    '0-20',
+    '21-30',
+    '31-40',
+    '41-50',
+    '51-100',
+  ]);
+
   readonly employees$: Observable<EmployeeModel[]> = combineLatest([
     this._employeesService.getAll(),
-    this.order$
+    this.order$,
+    this.ageRange$,
   ]).pipe(
-    map(([employees, order]: [EmployeeModel[], string]) => employees.sort((a, b) => {
-      if (a.salary > b.salary) return order === 'asc' ? 1 : -1;
-      if (a.salary < b.salary) return order === 'asc' ? -1 : 1;
-      return 0;
-    }))
-  )
+    map(([employees, order, ageRange]: [EmployeeModel[], string, AgeRange]) =>
+      employees
+        .filter(
+          (employee) =>
+            +employee.age >= +ageRange.minAge &&
+            +employee.age <= +ageRange.maxAge
+        )
+        .sort((a, b) => {
+          if (a.salary > b.salary) return order === 'asc' ? 1 : -1;
+          if (a.salary < b.salary) return order === 'asc' ? -1 : 1;
+          return 0;
+        })
+    )
+  );
 
-  constructor(private _employeesService: EmployeesService) {
-  }
+  constructor(private _employeesService: EmployeesService) {}
 
   sort(order: string): void {
     this._orderSubject.next(order);
+  }
+
+  selectAgeRange(ageRange: string): void {
+    if (ageRange !== 'All') {
+      const [minAge, maxAge] = ageRange.split('-');
+      console.log('minAge: ', minAge, ' maxAge: ', maxAge);
+      this._ageRangeSubject.next({ minAge: minAge, maxAge: maxAge });
+    } else {
+      this._ageRangeSubject.next({ minAge: '0', maxAge: '100' });
+    }
   }
 }
