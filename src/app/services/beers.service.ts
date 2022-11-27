d9cab6ebb80126085b02349906102cf98c31ebb5
lwalik <l.walicki1@gmail.com>
2022-11-28 00:41:38 +0100

    feat(advanced-lists-beers): beerModel and beersService created

---
+++ src/app/services/beers.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { BeerModel } from '../models/beer.model';
+
+@Injectable()
+export class BeersService {
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getAll(index: number, size: number): Observable<BeerModel[]> {
+    return this._httpClient.get<BeerModel[]>(`https://api.punkapi.com/v2/beers?page=${index}&per_page=${size}`);
+  }
+}
