6a6f6ff1fe79f0b08ab80e45b02c47347529717b
lwalik <l.walicki1@gmail.com>
2022-12-29 22:53:33 +0100

    feat(ex-9-search-ex-1-product-list): live reload search functionality

--- src/app/components/search-product-list/search-product-list.component.ts
+++ src/app/components/search-product-list/search-product-list.component.ts
@@ -4,7 +4,14 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { BehaviorSubject, combineLatest, map, Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  map,
+  Observable,
+  startWith,
+} from 'rxjs';
 import { ProductModel } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
 
@@ -15,30 +22,35 @@ import { ProductsService } from '../../services/products.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchProductListComponent {
-  private _searchSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
-    ''
-  );
-  public search$: Observable<string> = this._searchSubject.asObservable();
+  // private _searchSubject: BehaviorSubject<string> = new BehaviorSubject<string>(
+  //   ''
+  // );
+  // public search$: Observable<string> = this._searchSubject.asObservable();
   readonly searchForm: FormGroup = new FormGroup({
     title: new FormControl(''),
   });
 
   readonly list$: Observable<ProductModel[]> = combineLatest([
     this._productsService.getAll(),
-    this.search$,
+    this.searchForm.valueChanges.pipe(
+      startWith({ title: '' }),
+      debounceTime(1000)
+    ),
   ]).pipe(
     map(([products, search]) => {
-      if (!search) {
+      if (!search.title) {
         return [];
       }
 
-      return products.filter((product) => product.title.startsWith(search));
+      return products.filter((product) =>
+        product.title.startsWith(search.title)
+      );
     })
   );
 
   constructor(private _productsService: ProductsService) {}
 
   onSearchFormSubmitted(searchForm: FormGroup): void {
-    this._searchSubject.next(searchForm.value.title);
+    // this._searchSubject.next(searchForm.value.title);
   }
 }
