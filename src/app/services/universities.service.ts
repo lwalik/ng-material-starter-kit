c3b62d16d6be40050b25715684de48ba8a8d17ac
lwalik <l.walicki1@gmail.com>
2022-12-29 23:39:15 +0100

    feat(ex-9-search-ex-2-universities-list): university model and service created

---
+++ src/app/services/universities.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UniversityModel } from '../models/university.model';
+
+@Injectable({ providedIn: 'root' })
+export class UniversitiesService {
+  constructor(private _httpClient: HttpClient) {}
+
+  getAllByCountry(country: string): Observable<UniversityModel[]> {
+    return this._httpClient.get<UniversityModel[]>(
+      `http://universities.hipolabs.com/search?country=${country}`
+    );
+  }
+}
